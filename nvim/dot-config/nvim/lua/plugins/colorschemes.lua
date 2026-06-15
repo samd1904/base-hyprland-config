3108606b39a4ec7798c341771e52495409656554
Saumya Darne <samd1904@gmail.com>
2026-06-15 17:08:43 +0530

    nvim and kitty colorscheme updated

--- nvim/dot-config/nvim/lua/plugins/colorschemes.lua
+++ nvim/dot-config/nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,11 @@
 return {
   "sainnhe/edge",
   "rebelot/kanagawa.nvim",
+  "nyoom-engineering/oxocarbon.nvim",
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "oxocarbon",
     },
   },
 }
