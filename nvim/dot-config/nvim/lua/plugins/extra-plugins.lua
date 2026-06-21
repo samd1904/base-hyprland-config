3f6d2c8730c101d49f86c84bd7180d3bdea90e97
Saumya Darne <samd1904@gmail.com>
2026-06-21 11:40:52 +0530

    tex plugin added

--- nvim/dot-config/nvim/lua/plugins/extra-plugins.lua
+++ nvim/dot-config/nvim/lua/plugins/extra-plugins.lua
@@ -58,4 +58,13 @@ return {
       cursor_line_only = true,
     },
   },
+  {
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "zathura"
+    end,
+  },
 }
